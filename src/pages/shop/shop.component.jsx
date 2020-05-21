2efef48489c4c38fd5eee5a202bde40e9f5c54d5
iuliancarnaru <iulian.carnaru@gmail.com>
2020-05-21 08:28:42 +0100

    converting firebase call to promise

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -21,13 +21,19 @@ class ShopPage extends Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionsMap = convertCollectionSnapshotToMap(snapshot);
-        updateCollections(collectionsMap);
-        this.setState({ loading: false });
-      }
-    );
+    // -----> REST API approach
+    // fetch(
+    //   `https://firestore.googleapis.com/v1/projects/crwn-shop-3a030/databases/(default)/documents/collections`
+    // )
+    //   .then((response) => response.json())
+    //   .then((collections) => console.log(collections));
+
+    // -----> promise chain style
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
   }
 
   render() {
